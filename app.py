952c7bc0fd7e9872d2be2d3d0bd9a2a3a1dd1653
Tuba <spotiba@gmail.com>
2026-03-01 22:08:45 +0300

    Intro to html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
@@ -6,16 +6,7 @@ app = Flask(__name__)
 
 def home():
 
-    return "Welcome to Tubalar <h1>HELLO<h1>"
-
-@app.route('/<name>')
-def user(name):
-    return f"Helleu {name}!"
-
-@app.route('/admin')
-def admin():
-    return redirect(url_for("home"))
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
-    
