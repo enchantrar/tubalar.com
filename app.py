417b0fc3050df7901745a653ff69fd0ecd93e5be
Tuba <spotiba@gmail.com>
2026-03-01 21:01:10 +0300

    Intro to home and admin

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+
 app = Flask(__name__)
+
 @app.route('/')
+
 def home():
-    return 'Welcome to Tubalar'
+
+    return "Welcome to Tubalar <h1>HELLO<h1>"
+
+@app.route('/<name>')
+def user(name):
+    return f"Helleu {name}!"
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run()
+    
